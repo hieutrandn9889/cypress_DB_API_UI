e68e6cb8fd327dd695d3ac4eaac0c3640bef8f1a
Hieu Tran <hieu.tran@enouvo.com>
2022-06-03 11:32:57 +0700

    init project

---
+++ db_api_ui/cypress.config.js
@@ -0,0 +1,32 @@
+const { defineConfig } = require('cypress')
+
+module.exports = defineConfig({
+  projectId: '2pw1r7',
+  reporter: 'mochawesome',
+  reporterOptions: {
+    reportFilename: 'test-report',
+  },
+  retries: {
+    runMode: 2,
+    openMode: 0,
+  },
+  env: {
+    DB: {
+      user: 'myuser',
+      host: '127.0.0.1',
+      database: 'pokemon',
+      password: 'pass',
+      port: 32763,
+    },
+  },
+  e2e: {
+    // We've imported your old cypress plugins here.
+    // You may want to clean this up later by importing these.
+    setupNodeEvents(on, config) {
+      return require('./cypress/plugins/index.js')(on, config)
+    },
+    specPattern: 'cypress/e2e/**/*.{feature,features}',
+    excludeSpecPattern: ['**/stepDefinitions/*', '*.js', '*.md'],
+    baseUrl: 'https://www.google.co.in',
+  },
+})
