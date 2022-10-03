8f6ebe547261ccf7cf775fab77d83f498fc41854
Hieu Tran <hieu.tran@enouvo.com>
2022-10-03 17:23:58 +0700

    add step and base page

---
+++ db_api_ui/cypress/e2e/common/stepDefinitions/AnalyticsDatabae/analyticDatabaseChartStep.js
@@ -0,0 +1,92 @@
+import { Then } from "cypress-cucumber-preprocessor/steps";
+import AnalyticDatabaseChartPage from "../../../pages/AnalyticsDatabase/analyticDatabaseChartPage";
+
+Then("I enter url {string} to check", (url) => {
+  cy.wait(1000);
+  AnalyticDatabaseChartPage.enterUrlToAnalytic(url);
+});
+
+Then("I show url {string}", (url) => {
+  cy.wait(1000);
+  AnalyticDatabaseChartPage.showUrl(url);
+});
+
+Then("I input {string} in search textbox", (valueSearch) => {
+  cy.wait(1000);
+  AnalyticDatabaseChartPage.inputSearch(valueSearch);
+});
+
+Then("I input {string} in predicted target", (valuePredictedTarget) => {
+  cy.wait(1000);
+  AnalyticDatabaseChartPage.inputPredictedTarget(valuePredictedTarget);
+});
+
+Then("I input {string} in compared factors", (valueComparedFactors) => {
+  cy.wait(1000);
+  AnalyticDatabaseChartPage.inputComparedFactors(valueComparedFactors);
+});
+
+Then("I click on table in analysis", () => {
+  cy.wait(1000);
+  AnalyticDatabaseChartPage.clickTableAnalysis();
+});
+
+Then("I click on classification image", () => {
+  cy.wait(1000);
+  AnalyticDatabaseChartPage.clickClassificationAnalysis();
+});
+
+Then("I click on regression image", () => {
+  cy.wait(1000);
+  AnalyticDatabaseChartPage.clickRegressionAnalysis();
+});
+
+Then("I click on segmentation image", () => {
+  cy.wait(1000);
+  AnalyticDatabaseChartPage.clickSegmentationAnalysis();
+});
+
+Then("I click on correlational image", () => {
+  cy.wait(1000);
+  AnalyticDatabaseChartPage.clickCorrelationalAnalysis();
+});
+
+Then("I remove id", () => {
+  cy.wait(1000);
+  AnalyticDatabaseChartPage.removeId();
+});
+
+Then("I remove species", () => {
+  cy.wait(1000);
+  AnalyticDatabaseChartPage.removeSpecies();
+});
+
+Then("I click on run query button", () => {
+  cy.wait(1000);
+  AnalyticDatabaseChartPage.runQueryBtn();
+});
+
+Then("I verify the result of dataImpitation", () => {
+  cy.wait(1000);
+  AnalyticDatabaseChartPage.verifyResultDataImpitation();
+});
+
+Then("I verify the result of regressionClassification", () => {
+  cy.wait(1000);
+  AnalyticDatabaseChartPage.verifyResultRegressionClassification();
+});
+
+Then("I verify the result of segmentation", () => {
+  cy.wait(7000);
+  AnalyticDatabaseChartPage.verifyResultSegmentation();
+});
+
+Then("I verify the result of correlational", () => {
+  cy.wait(1000);
+  AnalyticDatabaseChartPage.verifyResultCorrelational();
+});
+
+Then("I verify the result of multivariateUnivariate", () => {
+  cy.wait(1000);
+  AnalyticDatabaseChartPage.verifyResultMultivariateUnivariate();
+});
---
+++ db_api_ui/cypress/e2e/pages/AnalyticsDatabase/analyticDatabaseChartPage.js
@@ -0,0 +1,100 @@
+import AnalyticDatabaseChartUI from "../../interface/analyticDatabaseChartUI";
+import BasePage from "../../intergration/basePage";
+
+export default class AnalyticDatabaseChartPage extends BasePage {
+  static enterUrlToAnalytic(url) {
+    this.enterUrl(url);
+  }
+
+  static showUrl(url) {
+    this.logUrl(url);
+  }
+
+  static inputSearch(valueSearch) {
+    this.typeElements(AnalyticDatabaseChartUI.inputSearch, valueSearch);
+    cy.wait(1000);
+  }
+
+  static inputPredictedTarget(valuePredictedTarget) {
+    this.typeElementsEnter(
+      AnalyticDatabaseChartUI.predictedTarget,
+      valuePredictedTarget
+    );
+    cy.wait(1000);
+  }
+
+  static inputComparedFactors(valuePredictedTarget) {
+    this.typeElementsEnter(
+      AnalyticDatabaseChartUI.comparedFactors,
+      valuePredictedTarget
+    );
+    cy.wait(1000);
+  }
+
+  static clickTableAnalysis() {
+    this.clickElements(AnalyticDatabaseChartUI.tableAnalysis);
+  }
+
+  static clickClassificationAnalysis() {
+    this.clickElements(AnalyticDatabaseChartUI.imgClassification);
+  }
+
+  static clickRegressionAnalysis() {
+    this.clickElements(AnalyticDatabaseChartUI.imgRegression);
+  }
+
+  static clickSegmentationAnalysis() {
+    this.clickElements(AnalyticDatabaseChartUI.imgSegmentationAnalysis);
+  }
+
+  static clickCorrelationalAnalysis() {
+    this.clickElements(AnalyticDatabaseChartUI.imgCorrelationalAnalysis);
+  }
+
+  static removeId() {
+    this.clickElements(AnalyticDatabaseChartUI.removeId);
+  }
+
+  static removeSpecies() {
+    this.clickElements(AnalyticDatabaseChartUI.removeSpecies);
+  }
+
+  static runQueryBtn() {
+    this.clickElements(AnalyticDatabaseChartUI.runQueryBtn);
+  }
+
+  static verifyResultCorrelational() {
+    this.verifyXpathVisibleElement(
+      AnalyticDatabaseChartUI.verifyResultCorrelational,
+      5000
+    );
+  }
+
+  static verifyResultDataImpitation() {
+    this.verifyXpathVisibleElement(
+      AnalyticDatabaseChartUI.verifyResultDataImpitation,
+      500000
+    );
+  }
+
+  static verifyResultRegressionClassification() {
+    this.verifyXpathVisibleElement(
+      AnalyticDatabaseChartUI.verifyResultRegressionClassification,
+      5000
+    );
+  }
+
+  static verifyResultSegmentation() {
+    this.verifyXpathVisibleElement(
+      AnalyticDatabaseChartUI.verifyResultSegmentation,
+      500000
+    );
+  }
+
+  static verifyResultMultivariateUnivariate() {
+    this.verifyXpathVisibleElement(
+      AnalyticDatabaseChartUI.verifyResultMultivariateUnivariate,
+      500000
+    );
+  }
+}
